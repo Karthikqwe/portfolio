c46614364136104cd5794f6106f68af74b184c62
Karthikqwe <daddakarthik97@gmail.com>
2025-02-24 11:52:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+greetings ();
+
+function   greetings () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+};
+let index=0;
+const span=document.querySelector(".developer");
+const text=document.querySelector(".developer").innerHTML;
+function showLetter(){
+    if(index<text.length){
+        span.textContent=text.slice(0,index+1);
+        index++;
+    }
+    else{
+        index=0;
+       showletter();
+    }
+}
+const interval = setInterval(showLetter, 300);
